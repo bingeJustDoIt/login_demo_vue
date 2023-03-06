1358552107fe9ec4f151a9c667367001ffa0de66
Binge <826819173@qq.com>
2023-03-06 15:13:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Login from "@/views/Login.vue";
+</script>
+<template>
+  <div id="app">
+    <Login/>
+<!--    <h2>测试</h2>-->
+  </div>
+</template>
+
+
+<style scoped lang="less">
+* {
+  box-sizing: border-box; /* 设置盒子模型为 border-box */
+  margin: 0; /* 清除默认的外边距 */
+  padding: 0; /* 清除默认的内边距 */
+}
+
+html, body {
+  height: 100%; /* 设置高度为 100% */
+  width: 100%; /* 设置宽度为 100% */
+}
+
+#app {
+  background-color: lightskyblue;
+  width: 100%; /* 设置宽度为 100% */
+  min-height: 100vh; /* 设置最小高度为 100vh */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="rules" label-width="60px">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">登录</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log(this.loginForm)
+      // this.$refs.loginForm.validate(valid => {
+      //   if (valid) {
+      //     // perform login action
+      //     console.log('Logged in!')
+      //   } else {
+      //     console.log('Error: form validation failed.')
+      //   }
+      // })
+    },
+    resetForm() {
+      this.loginForm.password = ''
+      this.loginForm.username = ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+.el-form {
+  //width: 400px;
+  //border: 1px solid black; /* 添加边框 */
+
+}
+
+.login-container {
+  background-color: skyblue;
+  width: 400px;
+  height: 400px;
+  border: 2px solid #ccc;
+  //padding: 10px;
+  border-radius: 5px;
+  padding: 30px;
+}
+
+</style>
